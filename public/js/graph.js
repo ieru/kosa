4a3f0b643f07a67923d7bbfb46189b353bcbe7d3
Fernando Sainz <fernando.sainz@afkartelecom.com>
2013-11-02 22:24:47 +0100

    Debugging v2

--- public/js/graph.js
+++ public/js/graph.js
@@ -2,7 +2,7 @@
 var labelType, useGradients, nativeTextSupport, animate;
 
 (function() {
-console.log("Inicializacion")
+console.log("Inicializacion");
   var ua = navigator.userAgent,
       iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
       typeOfCanvas = typeof HTMLCanvasElement,
@@ -766,7 +766,7 @@ function init(){
     //Create a new ST instance
     init();
     var st = new $jit.ST({
-        console.log("Inyeccion")
+        console.log("Inyeccion");
         //id of viz container element
         injectInto: 'navigational',
         //set duration for the animation
