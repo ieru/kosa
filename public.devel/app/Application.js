6f8447d4b6b904e6fa44b39bd575fccce1761cf6
Julian Romera Juarez <julian.romera@gmail.com>
2013-11-19 10:43:09 +0100

    concurrency problem workarround

--- public.devel/app/Application.js
+++ public.devel/app/Application.js
@@ -18,22 +18,12 @@ var Application = {
         // Imports
         var HomeView = require('views/HomeView');
         var ApplicationRouter = require('routers/ApplicationRouter');
-        var Events = require('events/Event');
-        // var GraphView = require('views/GraphView');
-        var GraphCollection = require('collections/GraphCollection');
+        // var Events = require('events/Event');
+        // var GraphCollection = require('collections/GraphCollection');
 
         // Initialize
-        // this.graphView = new GraphView();
         this.homeView = new HomeView();
         this.applicationRouter = new ApplicationRouter();
-
-        
-        /* 
-        if ($('#infovis').length > 0) {
-        this.graph.init();
-        this.homeView.initSearchBox();
-        }
-        */
 
 
         if (typeof Object.freeze === 'function') Object.freeze(this);
--- public.devel/app/initialize.js
+++ public.devel/app/initialize.js
@@ -17,9 +17,4 @@ $(function() {
 	// Start Backbone router
   	Backbone.history.start();
   	
-	// Non Backbone apps  	
-  	// if ($('#infovis').length > 0) {
-  	  // application.graphView.initGraph();
-  	  // application.homeView.initSearchBox();
-  	// }
 });
