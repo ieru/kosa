a31570d2c24ce868ecdbd4f5b1f2c6a9dcd5fa2a
Julian Romera Juarez <julian.romera@gmail.com>
2013-11-23 14:09:43 +0100

    clarifyied Back-end source code

--- lib/application.rb
+++ lib/application.rb
@@ -46,23 +46,6 @@ class Kosa < Sinatra::Base
   end
 
 
-    # RDF::Repository API:
- 
-    # repository.readable?
-    # repository.writable?
-    # repository.empty?
-    # repository.count  
-    # repository.has_statement?(statement)
-    # repository.each_statement { |statement| statement.inspect! }
-    # repository.insert(*statements)
-    # repository.insert(statement)
-    # repository.insert([subject, predicate, object])
-    # repository << statement
-    # repository << [subject, predicate, object]
-    # repository.delete(*statements)
-    # repository.delete(statement)
-    # repository.delete([subject, predicate, object])
-    # repository.clear!
  
     # Get Root (SPARQL 1.1)
     
@@ -88,10 +71,6 @@ class Kosa < Sinatra::Base
     end
 
 
-    #########################################################################################################
-    #########################################################################################################
-    
-    # api:
 
     # test endpoint1 
     get '/test' do
@@ -154,12 +133,14 @@ class Kosa < Sinatra::Base
            FILTER (!bound(?super)) .
          }
         ")
+        #.limit(1)
+         
         
          #  FILTER (langMatches(lang(?label), '#{lang}')) .           
         
 	
         root = query_children.each_solution do |w| 
-            {:w => w.label}.to_json
+            {:name => w.label, :id => remove_prefix(w.x)}.to_json
         end
         
         # {:name=>root, :id=>remove_prefix(root), :children=>[], :related=>[], :children_number=>0, :related_number=>0}.to_json
@@ -228,63 +209,14 @@ class Kosa < Sinatra::Base
         end
     
         
-        # Consult Adapter's Specific Respository syntax (http://rdf.rubyforge.org/RDF/Repository.html)
-        # to create queries. eg: DataObjects (SQLite or Postgres)->  http://rdf.rubyforge.org/do/RDF/DataObjects/Repository.html
-        # , AllegroGraph->http://rdf-agraph.rubyforge.org/ ... etc        
-        # AllegroGraph query syntax:
-        #
-        # list = repo.build_query do |q|
-        #    q.pattern [:subject, RDFS.subClassOf,  node.to_s]
-        # end.map do |u|
-        #   {:child => u.subject}
-        # end
-        
-        # should work (?)      
-        # list = repo.query([:subject, RDF::RDFS.subClassOf,  node]).map do |u|
-        # list = repo.query([:s, RDF::SKOS.broader , :o]).map { |w| {'a'=>w[0], 'b'=> w[1], 'c'=> w[2] }  }
-        
         node = remove_prefix(node)
         
         uri = prefix + '/' + node
         
-        # has_narrowers at AGROVOC:
-        # .../c_12848 --> c_1473
-
         if !uri.valid?
           {:name=>'', :id=>'', :children=>[], :related=>[], :children_number=>0, :related_number=>0}.to_json
         end
         
-        # query_children = sparql.query("CONSTRUCT WHERE { <"+ uri +"> skos:narrower ?o } LIMIT 10")
-	# c_1329377502888
-=begin
-	query_children = sparql.query("
-	PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
-	  CONSTRUCT {
-	    ?o ?label
-	  }
-	  WHERE { 
-	    <"+ uri +"> skos:broader ?o . 
-	    <"+ uri +"> skos:altLabel ?label . 
-	    } 
-	  LIMIT 10
-        ")
-
-query_children = sparql.query("
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
-SELECT ?s ?label ?o 
-WHERE
-{
-<#{uri}> skos:broader ?o .
-?o skos:altLabel ?label . FILTER(langMatches(lang(?label), '#{lang}')) . 
-FILTER isLiteral(?label) .
-}
-LIMIT 10
-")
-
-?x rdfs:subClassOf agrovoc:#{node} .
-
-=end
-
 query_children = sparql.query("
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX agrovoc: <#{prefix}/>
@@ -300,79 +232,15 @@ LIMIT 10
 ")
         
 	
-        # query_children = sparql.construct.where([:s, RDF::SKOS.broader, :o])
-	        
-        # query_children = RDF::Query.new({
-        #  :s => {
-        #       RDF::SKOS.broader  => :o,
-        #       RDF::SKOS.prefLabel => :label,
-        #   }
-        # })
-        #         
-        # query_children = RDF::Query.new do
-        #   pattern [:s, RDF::SKOS.narrower, :o]
-        #   pattern [:s, RDF::SKOS.prefLabel, :label]
-        # end
-        
-
-        #query_related = RDF::Query.new do
-        #  pattern [:s, RDF::SKOS.narrower, :o]
-        #end
-        
-        #pattern [:s, RDF::RDFS.label, :label, {:optional => true}]        
-        #pattern [:s, RDF::RDFS.subClassOf, :o]
-        #pattern [:s, RDF::RDFS.label, :label]
-        #pattern [:s, RDF::SKOS.narrower, :o]
-        
-        # list = repo.query([:s, RDF::RDFS.label, :slabel][:o, RDF::RDFS.label, :olabel][:s, RDF::SKOS.broader, :o]).map { |w| {'a'=>w[0], 'b'=> w[1], 'c'=> w[2] }  }
-        # list = query.execute(repo).map { |w| {'a'=>w[0], 'b'=> w[1], 'c'=> w[2] }  }
-        # list = query.execute(repo).map { |w| {'id'=>remove_prefix(w.s), 'child'=> w.o }  }
-        
-        # children = query_children
-        # children_count = children.execute(repo, {:o => uri}).filter{ |w|  w.name.language == lang }.count
         children_list = query_children.map { | w | { 
           :name=> w.label, :id=>remove_prefix(w.x), :children=>[], :related=>[], :children_number=>0, :related_number=>0 
         } }
         
-        # todo: language filter -> solutions.filter { |solution| solution.name.language == :es }
-        #list.first.s.to_uri.root.to_s + list.first.s.to_s
-                
-        # list.to_json
-        
         {:name=>uri, :id=>node, :children=>children_list, :related=>children_list, :children_number=>0, :related_number=>0}.to_json
-        # "#{prefix}/#{node}"
 
-        
-        # Cropontology Proxy   
-        # json_string = getJsonFromExternalAPI("http://www.cropontology.org/get-children/", node)
-        # parser = Yajl::Parser.new
-        # json = parser.parse(json_string)
-        # json.to_json
-        # # Siren.query("$..[? (@.name != null) & (@.children != null) & (@.children[0] != null) & (@.name = '"+node.to_s+"')][=children][0][? @.name != null][= name]", json).to_json
-      end
-
-    end
-
-    
-    # dummy method with test JSON data <--- this should be retrieved from database
-    def test_json
-      json_file = File.dirname(__FILE__) + "/../public/json/test_json.json"
-      if File.exists?(json_file)
-        json = File.read(json_file)
-        # json
-        return json
-      else 
-        return {}.to_json
       end
     end
 
-    # access external apis
-    def get_json_from_external_url(url, node)
-      url = url + node
-      resp = Net::HTTP.get_response(URI.parse(url))
-      return resp.body
-    end
-    
     # @todo: check this 
     # removes PREFIX from URIs 
     def remove_prefix(node)
--- public.devel/config.coffee
+++ public.devel/config.coffee
@@ -14,16 +14,14 @@ exports.config =
         'test/javascripts/test-vendor.js': /^test(\/|\\)(?=vendor)/
       order:
         before: [
-          'vendor/scripts/console-helper.js',
+          'vendor/scripts/console-helper.min.js',
           'vendor/scripts/jquery-1.10.2.min.js',
-          # 'vendor/scripts/underscore-1.3.3.js',
-          'vendor/scripts/lodash.custom.min.js',
-          'vendor/scripts/backbone-0.9.9.js',
-          'vendor/scripts/backbone-mediator.js',
-          'vendor/scripts/backbone.super.js',
-          'vendor/scripts/jit.js',
-          'vendor/scripts/select2.min.js'
-          # 'vendor/scripts/graph.js'
+          'vendor/scripts/lodash-custom-2.3.0.min.js',
+          'vendor/scripts/backbone-0.9.9.min.js',
+          'vendor/scripts/backbone-mediator.min.js',
+          # 'vendor/scripts/backbone.super.js',
+          'vendor/scripts/jit.min.js',
+          'vendor/scripts/select2-3.4.5.min.js'
         ]
         after: [
         ]
