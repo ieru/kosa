79c2cc76e562e3e0c228dec162ae9b3c5a3b6255
Julian Romera Juarez <julian.romera@gmail.com>
2013-12-02 07:29:10 +0100

    partially fixed germsplasm's ontology uris' issue. changed full uri sent as parameter: this allosws several ontologies at once on the Back. several sixes. code refactoring for clarity: for the programmer(s) supporting this code

--- lib/application.rb
+++ lib/application.rb
@@ -1,21 +1,15 @@
 # encoding: UTF-8
 
-# require 'rubygems'
-
 require 'sinatra'
-require 'open-uri'
-
-# todo remove
-#require 'sinatra/sparql'
-#require 'sinatra/respond_to'
+require 'uri'
 
 # rdf and rdf syntaxes
 require 'rdf'
 require 'rdf/turtle' 
 
-# database adapters
-#require 'rdf/4store'
+# Database adapters
 require 'rdf/sesame'
+#require 'rdf/4store'
 #require 'rdf-agraph'
 #require 'rdf/do'
 #require 'data_objects'
@@ -49,9 +43,10 @@ class Kosa < Sinatra::Base
   def initialize 
     
     # maximun number of result on query ~= 10pages
-    @soft_limit = 50
+    @soft_limit = 70
+    
     # elements on a tree level
-    @results_per_page = 5
+    @results_per_page = 2
     
     
     # @prefix = RDF::URI.new('http://aims.fao.org/aos/agrovoc')
@@ -59,39 +54,20 @@ class Kosa < Sinatra::Base
     
     url = "http://127.0.0.1:8888/openrdf-sesame/repositories/#{Repository}"
     @repo = RDF::Sesame::Repository.new(url)
-    # @repo = RDF::FourStore::Repository.new('http://localhost:8008/')
     
     @sparql = SPARQL::Client.new(repo)
     @root = ''
     @encoder = Yajl::Encoder.new
 
+    # repository string for other adpters    
     
+    # @repo = RDF::FourStore::Repository.new('http://localhost:8008/')
     # @repo = RDF::DataObjects::Repository.new('sqlite3:kosa.db')
     # repo = RDF::DataObjects::Repository.new 'postgres://postgres@server/database'
     # repo = RDF::DataObjects::Repository.new(ENV['DATABASE_URL']) #(Heroku)
     # url = "http://user:passwd@localhost:10035/repositories/example"
     # repo = RDF::AllegroGraph::Repository.new(url, :create => true)
   end
-
- 
-    # Get Root (SPARQL 1.1)
-    
-    # SELECT ?cls1
-    #  WHERE{
-    #    ?cls1 a owl:Class .
-    #    FILTER NOT EXISTS { ?cls1 rdfs:subClassOf ?cls2 .}
-    # }
-
-    # Get ROOT (SPARQL)
-    
-    # SELECT ?cls1
-    #  WHERE{
-    #     ?cls1 a owl:Class .
-    #     OPTIONAL{ ?cls1 rdfs:subClassOf ?cls2 .}
-    #     FILTER(!BOUND(?cls2))
-    # }
-
-
 
     # test endpoint1 
     get '/test' do
@@ -160,71 +136,14 @@ class Kosa < Sinatra::Base
           lang = lang.upcase
         end   
                 
-        # why doesnt this query work?     
-=begin
-        query_children = sparql.query("
-         PREFIX owl:  <http://www.w3.org/2002/07/owl#>
-         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-         SELECT DISTINCT ?x ?label
-         WHERE {
-           ?x a owl:Class .
-           ?x rdfs:label ?label .
-           OPTIONAL {
-             ?x rdfs:subClassOf ?super
-           }
-           FILTER (!bound(?super)) .
-         }
-        ").limit(1)
-=end 
-
-        # changed query for 'the first node which has children'
         query = sparql.query("
-         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-         SELECT REDUCED ?x ?label
-         WHERE
-         {
-         # ?x ?p ?o
-         # ?x skos:prefLabel ?label .
-         ?x rdfs:label ?label .
-         FILTER(langMatches(lang(?label), '#{lang}')) .
-         }
-         LIMIT 1
+        
+        #
+        # not implemented
+        #
         ")
-
-=begin
-        query = sparql.query("
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT ?concept ?p ?lcc
-        WHERE {  
-            ?concept ?p ?lcc.
-            OPTIONAL { ?concept skos:broader ?broader }
-            FILTER (!bound(?broader))
-        } LIMIT 1
-        ")
-=end
 
         
-        #query = RDF::Query.new({
-        #  :s => { RDF::SKOS.narrower => :o},
-        #  :o => { RDF::SKOS.prefLabel => :label}
-        #})
-
-        #pattern [:s, RDF::RDFS.label, :label, {:optional => true}]
-        #pattern [:s, RDF::RDFS.subClassOf, :o]
-        #pattern [:s, RDF::SKOS.prefLabel, :label]
-        #pattern [:s, RDF::RDFS.label, :label]
-        #pattern [:s, RDF::SKOS.narrower, :o]
-
-        # list = repo.query([:s, RDF::RDFS.label, :slabel][:o, RDF::RDFS.label, :olabel][:s, RDF::SKOS.broader, :o]).map { |w| {'a'=>w[0], 'b'=> w[1], 'c'=> w[2] }  }
-        # list = query.execute(repo).map { |w| {'a'=>w[0], 'b'=> w[1], 'c'=> w[2] }  }
-        # list = query.execute(repo).map { |w| {'id'=>remove_prefix(w.s), 'child'=> w.o }  }
-
-	#optimized_query = query.optimize!
-        #children_count = 0;
-        #root = optimized_query.execute(repo).distinct.limit(1).map { |w| {
-        #  :name=> w.label, :id=>remove_prefix(w.s), :children=>[], :related=>[], :children_number=>0, :related_number=>0
-        #} }
         root = query.map { |w|
           {:a => w }
         }
@@ -275,7 +194,6 @@ class Kosa < Sinatra::Base
     # passed type arg to Dry the method
     def get_concepts(type=nil,uri=nil, lang=nil, page=nil)
     
-      # encoder = Yajl::Encoder.new
 
         if uri.nil?
           # return null to save resources
@@ -283,7 +201,6 @@ class Kosa < Sinatra::Base
         end 
         
         if type.nil?
-          # return null to save resources
           type = 'narrower'
         end 
         
@@ -300,7 +217,7 @@ class Kosa < Sinatra::Base
           page = page.to_i
           if page < 1
             # stop execution to save resources
-            return encoder.encode({:error=>'Page not valid', :uri=>uri})
+            return encoder.encode({:error=>'Page not valid'})
           end
         end
         
@@ -314,7 +231,7 @@ class Kosa < Sinatra::Base
         
         if !rdf_uri.valid?
           # return empty, and stop to save resources
-          return encoder.encode({:error=>'not valid uri', :uri=>uri})
+          return encoder.encode({:error=>'Error validating Uri "#{uri}"'})
         end
 
         # url_uri = prefix + '/' + CGI::escape(node.to_s)
@@ -339,6 +256,7 @@ class Kosa < Sinatra::Base
           }
           # LIMIT #{soft_limit}
          ")
+         
          count = query.count()        
          parents_query = query.offset(offset).limit(results_per_page)
         
@@ -354,7 +272,7 @@ class Kosa < Sinatra::Base
 
         if page > pages
           # return empty, and stop to save resources
-          return encoder.encode({:uri=>uri})
+          return encoder.encode({:info=>'No data'})
         end
 
          ylabel = nil
@@ -364,11 +282,10 @@ class Kosa < Sinatra::Base
            if ylabel.nil?
              ylabel = w.ylabel
            end
-           id = 
-           { :name=> w.label, :id=>remove_prefix(w.x), :uri=>w.x.to_s, :pages=>0, :related_count=>0, :children=>[], :related=>[] }
+           { :name=> w.label, :id=>'', :uri=>w.x.to_s, :pages=>0, :related_count=>0, :children=>[], :related=>[] }
          } 
         
-         return encoder.encode({ :name=>ylabel, :id=>id, :uri=>uri.to_s, :pages=>pages, :page=>page, :related_count=>0, :children=>parents_list, :related=>parents_list })
+         return encoder.encode({ :name=>ylabel, :id=>'', :uri=>uri.to_s, :pages=>pages, :page=>page, :related_count=>0, :children=>parents_list, :related=>parents_list })
     end
 
     # @todo: check this 
